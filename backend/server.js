ba5dd59c365194039bf1db66d93964cfb7a91e71
Mitali-laroia <04042001mitali@gmail.com>
2021-06-30 06:50:24 +0530

    redux for fetching data from backend

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,9 @@
 import express from 'express'
-import products from './data/products.js'
-import connectDB from './config/db.js'
-import productRoutes from './routes/productRoutes.js'
-import { notFound,errorHandler } from './middleware/errorMiddleware.js'
 import dotenv from 'dotenv'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
+import connectDB from './config/db.js'
+
+import productRoutes from './routes/productRoutes.js'
 
 dotenv.config()
 
@@ -12,14 +12,19 @@ connectDB()
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('API is running...')
+  res.send('API is running....')
 })
 
-app.use('/api/products',productRoutes)
-
+app.use('/api/products', productRoutes)
 
 app.use(notFound)
 app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`))
+
+app.listen(
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`
+  )
+)
